83012f797c15c34d0908fe7cbd89409fda442e28
Unknown <alex.fullarton@live.com>
2020-02-26 14:30:29 +0000

    + Added enemy spawner object

--- Source/MyProject2/Map.h
+++ Source/MyProject2/Map.h
@@ -101,7 +101,7 @@ struct Map
 			m_corridors.clear();
 			m_blockages.clear();
 
-			AddRoomFromRoomData(start_room_5x5_1door, { (MAP_DIMENSIONS_X/2)-2, (MAP_DIMENSIONS_Y / 2) - 2 });
+			AddRoomFromRoomData(start_room_5x5_1door, { (MAP_DIMENSIONS_X/2) -1, (MAP_DIMENSIONS_Y / 2)-1 });
 
 			for (int i = 0; i < 300; i++)
 			{
--- Source/MyProject2/TestActor.cpp
+++ Source/MyProject2/TestActor.cpp
@@ -19,37 +19,6 @@ void ATestActor::BeginPlay()
 	Super::BeginPlay();
 
 	GetLevelManager()->CreateLevel(time(0));
-	//When weve got room data, spawn it somewhere
-	//SpawnRoom(room_data, grid_pos)
-
-	/*const float unreal_individual_tile_size = 100.f;
-	const float our_scale = 4.0f;
-	float room_size = 3.0f;
-	float offset = (room_size / 2.f) * (unreal_individual_tile_size * our_scale);
-
-	const float cell_to_unreal_size = (unreal_individual_tile_size * our_scale);
-
-	//Stuff that would be in room_data
-	int x1 = 0;
-	int y1 = 0;
-	int roomsize1 = 3;
-
-	//Take that out, scale it to unreal
-	float x1Scaled = x1 * cell_to_unreal_size;
-	float y1Scaled = y1 * cell_to_unreal_size;
-	float offset1 = ((float)roomsize1 / 2.f) * cell_to_unreal_size;
-
-	GetLevelManager()->SpawnBlueprintActor("Rooms/3x3_2door_straight_BP.3x3_2door_straight_BP'", { x1Scaled + offset1, y1Scaled + offset1, 0 }, FRotator(0, 0, 0));
-
-	//same for next room
-	int x2 = 3;
-	int y2 = 0;
-	int roomsize2 = 3;
-
-	float x2Scaled = x2 * cell_to_unreal_size;
-	float y2Scaled = y2 * cell_to_unreal_size;
-	float offset2 = ((float)roomsize2 / 2.f) * cell_to_unreal_size;
-	GetLevelManager()->SpawnBlueprintActor("Rooms/3x3_2door_straight_BP.3x3_2door_straight_BP'", { x2Scaled + offset2, y2Scaled + offset2, 0 }, FRotator(0, 0, 0));*/
 }
 
 // Called every frame
