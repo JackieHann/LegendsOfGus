fabc30f39ad2ab4e496d0ec4d754b25feffb3cc9
JackieHann <jake.hanne@googlemail.com>
2020-02-26 14:58:49 +0000

    +Base Character +Base GameMode +Key Mapping
    Starting to implement the base character movement and visual, also added the input for the controller

---
+++ Source/MyProject2/DungeonGameMode.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DungeonGameMode.h"
+#include "UObject/ConstructorHelpers.h"
+
+ADungeonGameMode::ADungeonGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Character/PlayerCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
