ad943152fe2268c9110407c05d379eac4e3616c8
Unknown <alex.fullarton@live.com>
2020-03-10 09:29:43 +0000

    * Collider added to enemies - root component changed

--- Source/MyProject2/Enemy.cpp
+++ Source/MyProject2/Enemy.cpp
@@ -14,17 +14,23 @@ AEnemy::AEnemy() :
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// add root component
-	RootComponent = CreateDefaultSubobject<USceneComponent>(FName("Enemy"));
-	// add capsule collider - needs to be resized on creation
-	Collider = CreateDefaultSubobject<UCapsuleComponent>(FName("Capsule Collider"));
-	// Attach capsule to root
-	Collider->AttachTo(RootComponent);
-	// Add mesh - will be changed to appropriate model
+	// add mesh
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Enemy Mesh"));
-	Mesh->AttachTo(RootComponent);
 	UStaticMesh* staticMesh = MeshAsset.Object;
 	Mesh->SetStaticMesh(staticMesh);
+	// set affected by gravity
+	Mesh->SetSimulatePhysics(true);
+	Mesh->SetEnableGravity(true);
+	// set mesh as root component
+	RootComponent = Mesh;
+	// add capsule collider to root and resize
+	Collider = CreateDefaultSubobject<UCapsuleComponent>(FName("Capsule Collider"));
+	Collider->InitCapsuleSize(100.0f, 100.0f);
+	Collider->AttachTo(RootComponent);
+	
+	
+
+	
 }
 
 // Called when the game starts or when spawned
